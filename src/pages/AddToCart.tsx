beb24ca3922f1bd69a2739eae744053377543da3
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-01-07 22:32:51 +0400

    add structure of AddToCart.tsx file

--- src/pages/AddToCart.tsx
+++ src/pages/AddToCart.tsx
@@ -1,7 +1,21 @@
+import Minus from "/icon-minus.svg";
+import Plus from "/icon-plus.svg";
+import Basket from "/icon-cart.svg";
+
 const AddToCart: React.FC = () => {
   return (
     <>
-      <p>Add to Cart</p>
+      <div>
+        <div>
+          <img src={Minus} alt="Minus" />
+          <span>0</span>
+          <img src={Plus} alt="Plus" />
+        </div>
+        <button>
+          <img src={Basket} alt="Basket" />
+          <span>Add to cart</span>
+        </button>
+      </div>
     </>
   );
 };
