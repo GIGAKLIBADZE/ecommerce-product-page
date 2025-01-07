ce517be57872446b7cec5418bcc04e3fc74163ca
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-01-07 20:22:26 +0400

    Initialize global styles

---
+++ src/GlobalStyles.tsx
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    *,
+    *::before,
+    *::after {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box; 
+    }
+
+    html {
+        font-size: 62.5%;
+        font-family: "Kumbh Sans";
+    }
+
+    body {
+        width: 100vw;
+        min-height: 100vh;
+        overflow-x: hidden;
+    }
+
+
+`;
+
+export default GlobalStyles;
